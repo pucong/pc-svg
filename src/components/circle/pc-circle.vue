5b63cb7dcd79ece52d6c6a6f44f7a6a3867d58c2
pucong <906886111@qq.com>
2017-10-24 22:57:38 +0800

    123

---
+++ src/components/circle/pc-circle.vue
@@ -0,0 +1,35 @@
+<template>
+  <outSvg>
+    <circle :cx="pcX" :cy="pcY" :r="pcR" stroke="black"
+            stroke-width="2" fill="red"/>
+  </outSvg>
+</template>
+<script>
+  import outSvg from '@/components/common/outSvg'
+
+  export default {
+    name: 'pc-circle',
+    props: {
+      pcX: { // 圆心的x坐标
+        type: Number,
+        default: 10
+      },
+      pcY: { // 圆心的y坐标
+        type: Number,
+        default: 10
+      },
+      pcR: { // 圆半径
+        type: Number,
+        default: 10
+      }
+    },
+    data () {
+      return {
+
+      }
+    },
+    components: {
+      outSvg
+    }
+  }
+</script>
--- src/components/left.vue
+++ src/components/left.vue
@@ -2,11 +2,7 @@
   <el-collapse v-model="activeNames">
     <el-collapse-item title="数据库" name="1">
       <el-card :body-style="{ padding: '0px' }">
-        <svg width="100%" height="100%" version="1.1"
-             xmlns="http://www.w3.org/2000/svg">
-          <circle cx="100" cy="50" r="40" stroke="black"
-                  stroke-width="2" fill="red"/>
-        </svg>
+        <pcCircle></pcCircle>
       </el-card>
     </el-collapse-item>
     <el-collapse-item title="图形" name="2">
@@ -18,12 +14,16 @@
   </el-collapse>
 </template>
 <script>
+  import pcCircle from '@/components/circle/pc-circle'
   export default {
     name: 'left',
     data () {
       return {
         activeNames: ['1']
       }
+    },
+    components: {
+      pcCircle
     }
   }
 </script>
